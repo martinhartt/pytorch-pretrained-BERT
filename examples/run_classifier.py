bd3c027f3d4d832fc4e6d4c6d43a183ef4fe3a4f
Martin Hartt <martinhartt1@gmail.com>
2018-11-28 09:25:01 +0000

    Do a fix

--- examples/run_classifier.py
+++ examples/run_classifier.py
@@ -126,7 +126,7 @@ class MrpcProcessor(DataProcessor):
     def _create_examples(self, lines, set_type):
         """Creates examples for the training and dev sets."""
         examples = []
-        for (i, line) in lines:
+        for i, line in lines:
             if i == 0:
                 continue
             guid = "%s-%s" % (set_type, i)
@@ -162,9 +162,7 @@ class GenericDataProcessor(DataProcessor):
     def _create_examples(self, lines, set_type):
         """Creates examples for the training and dev sets."""
         examples = []
-        for (i, line) in enumerate(lines):
-            if i == 0:
-                continue
+        for i, line in enumerate(lines):
             guid = "%s-%s" % (set_type, i)
             text_a = line["text"]
             label = line["tag"]
